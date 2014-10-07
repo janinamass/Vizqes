648deee5ab9933a0b0b94b563f8e18972f8c043e
gglyptodon <gglyptodon@gmail.com>
2014-10-07 17:18:57 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import glob
 from setuptools import setup, find_packages
 setup(
         name='seqPlot',
-        version='0.0.1',
+        version='0.1.0',
         author='Janina Mass',
         author_email='janina.mass@hhu.de',
         packages=['seqplot'],
@@ -12,8 +12,9 @@ setup(
         license='GPLv3',
         url='https://pypi.python.org/pypi/seqPlot/',
         description='Visualize (multiple) sequence alignment (MSA) with colored boxes',
-        long_description=open('README.md').read(),
-       	include_package_data=True, 
+        long_description=open('README.txt').read(),
+       	include_package_data=True,
+	install_requires=['Pillow'], 
 	classifiers=[
             'Programming Language :: Python :: 2.7',
             'Topic :: Scientific/Engineering :: Bio-Informatics'
